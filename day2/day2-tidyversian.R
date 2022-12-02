6f2a17411af027204a18885b64c15e94260eb5eb
Michal Bojanowski <michal2992@gmail.com>
2022-12-02 10:34:31 +0100

    Edits

--- day2/day2-tidyversian.R
+++ day2/day2-tidyversian.R
@@ -10,19 +10,15 @@ requireNamespace("here")
 
 strategy_guide <- readr::read_delim(here::here("day2", "day2-input.txt"),
                   col_types = "c",
-                  col_names = c("theirs", "ours"))
+                  col_names = c("v1", "v2"))
 
-strategy_guide |>
+d <- strategy_guide |>
   mutate(
-    s_theirs = match(theirs, LETTERS[1:3]),
-    s_ours = match(ours, LETTERS[24:26]),
-    s_outcome = c(3, 0, 6)[(3 + s_theirs - s_ours) %% 3 + 1],
+    s_theirs = match(v1, LETTERS[1:3]),
+    s_ours = match(v2, LETTERS[24:26]),
+    outcome = (3 + s_theirs - s_ours) %% 3,
+    s_outcome = c(3, 0, 6)[outcome + 1],
     pts = s_ours + s_outcome
-  ) |>
-  with(sum(pts))
+  )
 
-
-
-# Part 2 ------------------------------------------------------------------
-
-
+with(d, sum(pts))
